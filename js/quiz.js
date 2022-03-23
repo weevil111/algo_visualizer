ea9b4a3c1281e87ada202b6d05a13fffa1addbfd
Abhinav Mishra <--global>
2022-03-23 22:08:36 +0530

    Fetch questions of selected algorithms

---
+++ js/quiz.js
@@ -0,0 +1,25 @@
+let quizList = [];
+
+async function fetchQuiz() {
+  const selectedAlgos = JSON.parse(localStorage.getItem("selectedAlgos"));
+  if (!selectedAlgos) {
+    alert("No Algo selected ! Please select one");
+    window.location.href = '/begin-quiz.html'
+  }
+  await fillQuizList(selectedAlgos);
+}
+
+async function fillQuizList(selectedAlgos) {
+  try {
+    for (const algo of selectedAlgos) {
+      const snapshot = await firestore.collection(`questions/sorting/${algo}`).get()
+      snapshot.forEach((doc) => {
+        quizList.push(doc.data())
+      });
+    }
+  } catch (err) {
+    console.log("Couldn't fetch questions ", err);
+  }
+}
+
+fetchQuiz()
