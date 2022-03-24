5c3dfea910b827b9b1b38e45975ff9d562434142
Abhinav Mishra <--global>
2022-03-24 20:01:41 +0530

    Design fix for quiz and fill first question on load

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,10 @@
+const quesNumberEl = document.querySelector(".question-no");
+const questionStatementEl = document.querySelector(".quiz-title");
+const answerOptionsArray = Array.from(document.querySelectorAll(".quiz-answer"));
+
+const TOTAL_QUESTIONS = 10;
 let quizList = [];
+let currentQuestionNumber = 0;
 
 async function fetchQuiz() {
   const selectedAlgos = JSON.parse(localStorage.getItem("selectedAlgos"));
@@ -10,7 +16,10 @@ async function fetchQuiz() {
 
   // Shuffle the quiz list:
   quizList.sort(() => Math.random() > 0.5 ? 1 : -1)
+  quizList = quizList.slice(0, TOTAL_QUESTIONS)
 
+  // Set the first question:
+  nextQuestion()
 }
 
 async function fillQuizList(selectedAlgos) {
@@ -27,6 +36,24 @@ async function fillQuizList(selectedAlgos) {
   } catch (err) {
     console.log("Couldn't fetch questions ", err);
   }
+}
+
+
+function nextQuestion() {
+  if (currentQuestionNumber === TOTAL_QUESTIONS) return;
+
+  currentQuestionNumber += 1;
+  const currentQuestion = quizList[currentQuestionNumber];
+  quesNumberEl.innerText = `Question ${currentQuestionNumber}/${TOTAL_QUESTIONS}`;
+  questionStatementEl.innerText = currentQuestion.question;
+  answerOptionsArray.forEach((el, index) => {
+    const option = currentQuestion.options[index];
+    el.innerHTML = `
+    <input type="radio" name="option" id="${option.id}">
+    ${option.value}
+    `
+  })
+
 }
 
 fetchQuiz()
