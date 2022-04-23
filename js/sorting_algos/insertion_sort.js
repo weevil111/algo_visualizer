5b5b664c87970110a5e7aa872940a6031526d198
Abhinav Mishra <--global>
2022-04-23 22:09:23 +0530

    Add pseudo code visualization for insertion sort

--- js/sorting_algos/insertion_sort.js
+++ js/sorting_algos/insertion_sort.js
@@ -1,21 +1,21 @@
 function Insertion() {
     c_delay = 0;
-
+    highlightLine({ 1: "red" })
     for (var j = 0; j < array_size; j++) {
-        div_update(divs[j], div_sizes[j], "#8e44ad");//Color update
+        div_update(divs[j], div_sizes[j], "#8e44ad", { 2: "#8e44ad", 3: "red" });//Color update
 
         var key = div_sizes[j];
         var i = j - 1;
         while (i >= 0 && div_sizes[i] > key) {
-            div_update(divs[i], div_sizes[i], "red");//Color update
+            div_update(divs[i], div_sizes[i], "red", { 4: "red" });//Color update
             div_update(divs[i + 1], div_sizes[i + 1], "red");//Color update
 
             div_sizes[i + 1] = div_sizes[i];
 
-            div_update(divs[i], div_sizes[i], "red");//Height update
+            div_update(divs[i], div_sizes[i], "red", { 4: "#e67e22" });//Height update
             div_update(divs[i + 1], div_sizes[i + 1], "red");//Height update
 
-            div_update(divs[i], div_sizes[i], "#e67e22");//Color update
+            div_update(divs[i], div_sizes[i], "#e67e22", { 5: "red" });//Color update
             if (i == (j - 1)) {
                 div_update(divs[i + 1], div_sizes[i + 1], "#8e44ad");//Color update
             }
@@ -27,7 +27,9 @@ function Insertion() {
         div_sizes[i + 1] = key;
 
         for (var t = 0; t < j; t++) {
-            div_update(divs[t], div_sizes[t], "green");//Color update
+            const pseudoCodePayload = { 6: "red" }
+            if (t == j - 1) pseudoCodePayload[7] = "green"
+            div_update(divs[t], div_sizes[t], "green", pseudoCodePayload);//Color update
         }
     }
     div_update(divs[j - 1], div_sizes[j - 1], "green");//Color update
