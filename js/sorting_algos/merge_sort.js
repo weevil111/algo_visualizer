52873d77d9c911a8a976df8043119ace86ef2f0e
Abhinav Mishra <--global>
2022-04-24 13:32:34 +0530

    Add pseudo code visualisation for merge sort

--- js/sorting_algos/merge_sort.js
+++ js/sorting_algos/merge_sort.js
@@ -14,32 +14,32 @@ function merge_sort(start, mid, end) {
     for (var i = start; i <= end; i++) {
         if (p > mid) {
             Arr[k++] = div_sizes[q++];
-            div_update(divs[q - 1], div_sizes[q - 1], "red");//Color update
+            div_update(divs[q - 1], div_sizes[q - 1], "red", { 3: "#e67e22", 6: "red" });//Color update
         }
         else if (q > end) {
             Arr[k++] = div_sizes[p++];
-            div_update(divs[p - 1], div_sizes[p - 1], "red");//Color update
+            div_update(divs[p - 1], div_sizes[p - 1], "red", { 3: "#e67e22", 4: "red", 5: "red" });//Color update
         }
         else if (div_sizes[p] < div_sizes[q]) {
             Arr[k++] = div_sizes[p++];
-            div_update(divs[p - 1], div_sizes[p - 1], "red");//Color update
+            div_update(divs[p - 1], div_sizes[p - 1], "red", { 3: "#e67e22", 4: "red", 5: "red" });//Color update
         }
         else {
             Arr[k++] = div_sizes[q++];
-            div_update(divs[q - 1], div_sizes[q - 1], "red");//Color update
+            div_update(divs[q - 1], div_sizes[q - 1], "red", { 3: "#e67e22", 6: "red" });//Color update
         }
     }
 
     for (var t = 0; t < k; t++) {
         div_sizes[start++] = Arr[t];
-        div_update(divs[start - 1], div_sizes[start - 1], "green");//Color update
+        div_update(divs[start - 1], div_sizes[start - 1], "green", { 7: "green", });//Color update
     }
 }
 
 function merge_partition(start, end) {
     if (start < end) {
         var mid = Math.floor((start + end) / 2);
-        div_update(divs[mid], div_sizes[mid], "#8e44ad");//Color update
+        div_update(divs[mid], div_sizes[mid], "#8e44ad", { 1: "#8e44ad" });//Color update
 
         merge_partition(start, mid);
         merge_partition(mid + 1, end);
